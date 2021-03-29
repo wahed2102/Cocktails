bbaf7dc101b2b5f7246c51596e6cd848bcca5764
wahed2102 <wahed.khan@rediffmail.com>
2021-03-30 03:23:17 +0530

    Navbar

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,16 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur fuga
+        veniam voluptatibus corporis nam delectus dicta corrupti beatae eum
+        adipisci expedita tenetur aperiam maiores exercitationem perferendis
+        nihil quidem, maxime sint autem illum, nesciunt odit voluptatum? Ipsam
+        nostrum nam laudantium inventore.
+      </p>
+    </section>
   );
 }
 
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Error() {
   return (
-    <div>
-      <h1>Error Page</h1>
-    </div>
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>oops! it's a dead end</h1>
+        <Link to="/" className="btn btn-primary">
+          back home
+        </Link>
+      </div>
+    </section>
   );
 }
 
